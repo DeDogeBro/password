a736cec980441f907535416dcae548910841f39b
jasonjanuary2011 <92645207+jasonjanuary2011@users.noreply.github.com>
2023-05-21 15:58:18 -0700

    Add files via upload

---
+++ eeeeeeeeeeeeeee.java
@@ -0,0 +1,11 @@
+index.js
+var password = "elvinistiny1!";
+function passcheck(){
+if(document.getElementById('pass1').value != password){
+alert('wrong password try again :D');
+return false;
+}   
+if(document.getElementById('pass1').value == password){
+alert('niceee go to https;//infmapi.github.io/infmapr.github.io/retrogames/launcher.html#pokemonemerald and do not tell anyone ok?');
+}    
+} 
